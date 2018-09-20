4acb600bbc812ca937d0964cdf31faf6be3584ee
krishnanselva <krishnanselva@yahoo.com>
2018-09-20 21:12:37 +0100

    controller failing unit tests

--- src/main/java/com/abc/account/controller/AccountController.java
+++ src/main/java/com/abc/account/controller/AccountController.java
@@ -1,10 +1,11 @@
 package com.abc.account.controller;
 
 import com.abc.account.domain.Account;
-import com.abc.account.exception.AccountException;
+import com.abc.account.service.AccountService;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +16,19 @@ import java.util.List;
 @RequestMapping("/account-project/rest/account/json")
 public class AccountController {
     Logger logger = LoggerFactory.getLogger(AccountController.class);
+    @Autowired
+    private AccountService accountService;
 
     @ApiOperation(value = "Get All Accounts")
     @GetMapping
     public ResponseEntity<List<Account>> getAllAccounts() {
-       return null;
+        return null;
 
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handleException(AccountException ae) {
-        logger.info(ae.toString(), ae);
+    public void handleException(Exception e) {
+        logger.info(e.toString(), e);
     }
 }
--- src/main/java/com/abc/account/service/AccountService.java
+++ src/main/java/com/abc/account/service/AccountService.java
@@ -2,9 +2,11 @@ package com.abc.account.service;
 
 import com.abc.account.domain.Account;
 import com.abc.account.exception.AccountException;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class AccountService {
     public List<Account> findAllAccounts() throws AccountException {
         return null;
